54c8719a78a037d60d2dc126a1e3b12ea63e560b
Bobby Pine <rob.rudary@gmail.com>
2020-07-09 11:13:01 -0400

    Opens cart page

--- Kith.py
+++ Kith.py
@@ -3,11 +3,15 @@ import json
 import requests
 import time
 import urllib3
+import logging
 
 urllib3.disable_warnings()
 
 
 def keysearch(keyword, size):
+    logging.basicConfig(level=logging.INFO, filename='Kith_Log.log', filemode='w',
+                        format = " %(asctime)s: (%(filename)s): %(levelname)s: %(funcName)s Line: %(lineno)d - %(message)s",
+                        datefmt="%m/%d/%Y %I:%M:%S %p ")
     starttime = time.time()
     url = 'https://www.kith.com/products.json'
     response = requests.get(url=url, verify=False)
@@ -18,18 +22,20 @@ def keysearch(keyword, size):
     for items in data['products']:
         if keyword in items['title'].lower():
             mylist.append(items['title'])
+            product = items['title']
             print(items['title'], 'https://kith.com/products/{}'.format(items['handle']))
             itemurl = 'https://kith.com/products/{}'.format(items['handle'])
             #webbrowser.open('https://kith.com/products/{}'.format(items['handle']))
             print('Product Found at {} in {:.2f} Seconds'.format(time.strftime("%I:%M:%S"),time.time() - starttime))
-            print('Adding to cart...')
-            print('Taking you to queue...')
+            print('Generating Cart Link...')
+            #print('Taking you to queue...')
             url2 = '{}.json'.format(itemurl)
             response2 = requests.get(url=url2, verify=False)
             data2 = json.loads(response2.content.decode('utf-8'))
             for sizes in data2['product']['variants']:
                 if sizes['title'] == size:
-                    carturl = 'https://kith.com/cart/{}:1'.format(sizes['id'])
+                    carturl = 'https://kith.com/cart/add?id={}'.format(sizes['id'])
+                    logging.info(f'Cart URL for {product}: {carturl}')
                     print('Cart Link: {}'.format(carturl))
                     webbrowser.open(carturl)
 
